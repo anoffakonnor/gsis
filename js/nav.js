8ae927d7edc8a0d42451c82d6127a73dbfd01dd2
Emmanuel A. Anoff <155363546+anoffakonnor@users.noreply.github.com>
2024-06-22 19:46:51 +0100

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+const primaryNav = document.querySelector(".nav-list");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", "true")
+    }
+})
